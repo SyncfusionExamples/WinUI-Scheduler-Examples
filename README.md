f4f3ecc74619853460de15f7e7d25cef49d16af1
AzureAD\KarthickMani <karthick.mani@syncfusion.com>
2021-03-19 11:11:23 +0530

    WINUI-869 - Integrating getting started and data binding demos

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # syncfusion WinUi Scheduler control examples
 
 This repository contains the samples that demonstrate the functionalities of WinUI Scheduler control.
+
+# getting-started-demo
+This sample demonstrates how to add events in Scheduler
+
+# data-binding-demo
+This sample demonstrates about mapping business objects to schedule appointments
---
+++ data-binding-demo/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Schedule View Model
+    /// </summary>
+    public class SchedulerViewModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// current day meetings 
+        /// </summary>
+        private List<string> currentDayMeetings;
+
+        /// <summary>
+        /// minimum time meetings
+        /// </summary>
+        private List<string> minTimeMeetings;
+        /// <summary>
+        /// color collection
+        /// </summary>
+        private List<Brush> colorCollection;
+
+        #endregion Properties
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScheduleViewModel" /> class.
+        /// </summary>
+        public SchedulerViewModel()
+        {
+            this.Appointments = new ObservableCollection<SchedulerModel>();
+            this.InitializeDataForBookings();
+            this.IntializeAppoitments();
+        }
+
+        #endregion Constructor
+
+        /// <summary>
+        /// Gets or sets appointments
+        /// </summary>
+        public ObservableCollection<SchedulerModel> Appointments
+        {
+            get;
+            set;
+        }
+
+        #region Methods
+
+        #region GettingTimeRanges
+
+        /// <summary>
+        /// Method for get timing range.
+        /// </summary>
+        /// <returns>return time collection</returns>
+        private List<Point> GettingTimeRanges()
+        {
+            List<Point> randomTimeCollection = new List<Point>();
+            randomTimeCollection.Add(new Point(9, 11));
+            randomTimeCollection.Add(new Point(12, 14));
+            randomTimeCollection.Add(new Point(15, 17));
+
+            return randomTimeCollection;
+        }
+
+        #endregion GettingTimeRanges
+
+        #region InitializeDataForBookings
+
+        /// <summary>
+        /// Method for initialize data bookings.
+        /// </summary>
+        private void InitializeDataForBookings()
+        {
+            this.currentDayMeetings = new List<string>();
+            this.currentDayMeetings.Add("General Meeting");
+            this.currentDayMeetings.Add("Plan Execution");
+            this.currentDayMeetings.Add("Project Plan");
+            this.currentDayMeetings.Add("Consulting");
+            this.currentDayMeetings.Add("Performance Check");
+            this.currentDayMeetings.Add("Yoga Therapy");
+            this.currentDayMeetings.Add("Plan Execution");
+            this.currentDayMeetings.Add("Project Plan");
+            this.currentDayMeetings.Add("Consulting");
+            this.currentDayMeetings.Add("Performance Check");
+
+            // MinimumHeight Appointment Subjects
+            this.minTimeMeetings = new List<string>();
+            this.minTimeMeetings.Add("Client Metting");
+            this.minTimeMeetings.Add("Birthday wish alert");
+
+            this.colorCollection = new List<Brush>();
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 133, 81, 242)));
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 140, 245, 219)));
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 83, 99, 250)));
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 255, 222, 133)));
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 45, 153, 255)));
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 253, 183, 165)));
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 198, 237, 115)));
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 253, 185, 222)));
+            this.colorCollection.Add(new SolidColorBrush(Color.FromArgb(255, 83, 99, 250)));
+        }
+
+        #endregion InitializeDataForBookings
+
+        #region InitializeAppointments
+        /// <summary>
+        /// Initialize appointments
+        /// </summary>
+        /// <param name="count">count value</param>
+        private void IntializeAppoitments()
+        {
+            Random randomTime = new Random();
+            List<Point> randomTimeCollection = this.GettingTimeRanges();
+
+            DateTime date;
+            DateTime dateFrom = DateTime.Now.AddDays(-100);
+            DateTime dateTo = DateTime.Now.AddDays(100);
+            var random = new Random();
+            var dateCount = random.Next(4);
+            DateTime dateRangeStart = DateTime.Now.AddDays(0);
+            DateTime dateRangeEnd = DateTime.Now.AddDays(1);
+
+            for (date = dateFrom; date < dateTo; date = date.AddDays(1))
+            {
+                if (date.Day % 7 != 0)
+                {
+                    for (int additionalAppointmentIndex = 0; additionalAppointmentIndex < 1; additionalAppointmentIndex++)
+                    {
+                        SchedulerModel meeting = new SchedulerModel();
+                        int hour = randomTime.Next((int)randomTimeCollection[additionalAppointmentIndex].X, (int)randomTimeCollection[additionalAppointmentIndex].Y);
+                        meeting.From = new DateTime(date.Year, date.Month, date.Day, hour, 0, 0);
+                        meeting.To = meeting.From.AddHours(1);
+                        meeting.EventName = this.currentDayMeetings[randomTime.Next(9)];
+                        meeting.Color = this.colorCollection[randomTime.Next(9)];
+                        meeting.ForegroundColor = GetAppointmentForeground(meeting.Color);
+                        meeting.IsAllDay = false;
+                        meeting.StartTimeZone = string.Empty;
+                        meeting.EndTimeZone = string.Empty;
+                        this.Appointments.Add(meeting);
+                    }
+                }
+                else
+                {
+                    SchedulerModel meeting = new SchedulerModel();
+                    meeting.From = new DateTime(date.Year, date.Month, date.Day, randomTime.Next(9, 11), 0, 0);
+                    meeting.To = meeting.From.AddDays(2).AddHours(1);
+                    meeting.EventName = this.currentDayMeetings[randomTime.Next(9)];
+                    meeting.Color = this.colorCollection[randomTime.Next(9)];
+                    meeting.ForegroundColor = GetAppointmentForeground(meeting.Color);
+                    meeting.IsAllDay = true;
+                    meeting.StartTimeZone = string.Empty;
+                    meeting.EndTimeZone = string.Empty;
+                    this.Appointments.Add(meeting);
+                }
+            }
+
+            // Minimum Height Meetings
+            DateTime minDate;
+            DateTime minDateFrom = DateTime.Now.AddDays(-2);
+            DateTime minDateTo = DateTime.Now.AddDays(2);
+
+            for (minDate = minDateFrom; minDate < minDateTo; minDate = minDate.AddDays(1))
+            {
+                SchedulerModel meeting = new SchedulerModel();
+                meeting.From = new DateTime(minDate.Year, minDate.Month, minDate.Day, randomTime.Next(9, 18), 30, 0);
+                meeting.To = meeting.From;
+                meeting.EventName = this.minTimeMeetings[randomTime.Next(0, 1)];
+                meeting.Color = this.colorCollection[randomTime.Next(0, 2)];
+                meeting.ForegroundColor = GetAppointmentForeground(meeting.Color);
+                meeting.StartTimeZone = string.Empty;
+                meeting.EndTimeZone = string.Empty;
+
+                this.Appointments.Add(meeting);
+            }
+        }
+
+        /// <summary>
+        /// Method to get foreground color based on background.
+        /// </summary>
+        /// <param name="backgroundColor"></param>
+        /// <returns></returns>
+        private Brush GetAppointmentForeground(Brush backgroundColor)
+        {
+            if ((backgroundColor as SolidColorBrush).Color.ToString().Equals("#FF8551F2") || (backgroundColor as SolidColorBrush).ToString().Equals("#FF5363FA") || (backgroundColor as SolidColorBrush).ToString().Equals("#FF2D99FF"))
+                return new SolidColorBrush(Microsoft.UI.Colors.White);
+            else
+                return new SolidColorBrush(Color.FromArgb(255, 51, 51, 51));
+        }
+
+        #endregion InitializeAppointments
+        #endregion Methods
+    }
+}
