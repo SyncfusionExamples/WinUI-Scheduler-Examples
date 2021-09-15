0f606c1c96ab0c2885c04a5c2f683070270db407
Naveenklumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2021-09-15 12:15:17 +0530

    Added new samples for 2021 volume 3 features

---
+++ AppointmentSelectionBorderBrush/AppointmentSelectionBorderBrush/ViewModel/ViewModel.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.UI.Xaml.Media;
+using Syncfusion.UI.Xaml.Core;
+using Syncfusion.UI.Xaml.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace AppointmentSelectionBorderBrush
+{
+    public class ViewModel : NotificationObject
+    {
+        public ViewModel()
+        {
+            Events = GenerateRandomAppointments();
+            DisplayDate = DateTime.Now.Date.AddHours(9);
+        }
+
+        private ScheduleAppointmentCollection events;
+
+        public DateTime DisplayDate { get; set; }
+
+        public ScheduleAppointmentCollection Events
+        {
+            get { return events; }
+            set
+            {
+                events = value;
+                RaisePropertyChanged("Events");
+            }
+        }
+
+        private ScheduleAppointmentCollection GenerateRandomAppointments()
+        {
+            var WorkWeekDays = new ObservableCollection<DateTime>();
+            var WorkWeekSubjects = new ObservableCollection<string>()
+                                                           { "GoToMeeting", "Business Meeting", "Conference", "Project Status Discussion",
+                                                             "Auditing", "Client Meeting", "Generate Report", "Target Meeting", "General Meeting" };
+
+            var NonWorkingDays = new ObservableCollection<DateTime>();
+
+            var YearlyOccurranceSubjects = new ObservableCollection<string>() { "Wedding Anniversary", "Sam's Birthday", "Jenny's Birthday" };
+            var MonthlyOccurranceSubjects = new ObservableCollection<string>() { "Pay House Rent", "Car Service", "Medical Check Up" };
+            var WeekEndOccurranceSubjects = new ObservableCollection<string>() { "FootBall Match", "TV Show" };
+
+            var brush = new ObservableCollection<SolidColorBrush>();
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 157, 101, 201)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 240, 138, 93)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 103, 155, 155)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 141, 34, 45)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 22, 146, 22)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 12, 22, 216)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 216, 145, 12)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 177, 7, 191)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 39, 170, 254)));
+            Random ran = new Random();
+            DateTime today = DateTime.Now;
+            if (today.Month == 12)
+            {
+                today = today.AddMonths(-1);
+            }
+            else if (today.Month == 1)
+            {
+                today = today.AddMonths(1);
+            }
+
+            DateTime startMonth = new DateTime(today.Year, today.Month - 1, 1, 0, 0, 0);
+            int day = (int)startMonth.DayOfWeek;
+            DateTime CurrentStart = startMonth.AddDays(-day);
+
+            var appointments = new ScheduleAppointmentCollection();
+            for (int i = 0; i < 90; i++)
+            {
+                if (i % 7 == 0 || i % 7 == 6)
+                {
+                    //add weekend appointments
+                    NonWorkingDays.Add(CurrentStart.AddDays(i));
+                }
+                else
+                {
+                    //Add Workweek appointment
+                    WorkWeekDays.Add(CurrentStart.AddDays(i));
+                }
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                DateTime date = WorkWeekDays[ran.Next(0, WorkWeekDays.Count)].AddHours(ran.Next(9, 17));
+                appointments.Add(new ScheduleAppointment()
+                {
+                    StartTime = date,
+                    EndTime = date.AddHours(1),
+                    AppointmentBackground = brush[i % brush.Count],
+                    Subject = WorkWeekSubjects[i % WorkWeekSubjects.Count]
+                });
+            }
+            int j = 0;
+            int k = 0;
+            int l = 0;
+
+            while (j < YearlyOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)];
+                appointments.Add(new ScheduleAppointment()
+                {
+                    StartTime = date,
+                    EndTime = date.AddHours(1),
+                    AppointmentBackground = brush[1],
+                    Subject = YearlyOccurranceSubjects[j]
+                });
+                j++;
+            }
+            while (k < MonthlyOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)].AddHours(ran.Next(9, 23));
+                appointments.Add(new ScheduleAppointment()
+                {
+                    StartTime = date,
+                    EndTime = date.AddHours(1),
+                    AppointmentBackground = brush[k],
+                    Subject = MonthlyOccurranceSubjects[k]
+                });
+                k++;
+            }
+            while (l < WeekEndOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)].AddHours(ran.Next(0, 23));
+                appointments.Add(new ScheduleAppointment()
+                {
+                    StartTime = date,
+                    EndTime = date.AddHours(1),
+                    AppointmentBackground = brush[l],
+                    Subject = WeekEndOccurranceSubjects[l]
+                });
+                l++;
+            }
+
+            return appointments;
+        }
+    }
+}
---
+++ ResourceAutoRowHeight/ResourceAutoRowHeight/ViewModel/ViewModel.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+using Syncfusion.UI.Xaml.Core;
+using Syncfusion.UI.Xaml.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.UI;
+
+namespace ResourceAutoRowHeight
+{
+    public class ViewModel : NotificationObject
+    {
+        private ScheduleAppointmentCollection events;
+
+        private ObservableCollection<object> resources;
+
+        private ObservableCollection<SolidColorBrush> brushes;
+
+        public ViewModel()
+        {
+            GetBrushCollection();
+            InitializeResources();
+            GenerateRandomAppointments();
+            DisplayDate = DateTime.Now.Date.AddHours(9);
+        }
+
+        public DateTime DisplayDate { get; set; }
+
+        public ScheduleAppointmentCollection Events
+        {
+            get { return events; }
+            set
+            {
+                events = value;
+                RaisePropertyChanged("Events");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets resource collection.
+        /// </summary>
+        public ObservableCollection<object> Resources
+        {
+            get
+            {
+                return resources;
+            }
+
+            set
+            {
+                resources = value;
+                this.RaisePropertyChanged("Resources");
+            }
+        }
+
+        private void InitializeResources()
+        {
+            this.Resources = new ObservableCollection<object>();
+            var nameCollection = new List<string>()
+            {
+                "Sophia",
+                "Daniel",
+                "James William",
+                "Kinsley Ruby",
+                "Emilia William",
+                "Stephen",
+                "Zoey Addison",
+            };
+
+            for (int i = 0; i <6; i++)
+            {
+                SchedulerResource employee = new SchedulerResource();
+                employee.Name = nameCollection[i];
+                employee.Background = this.brushes[i];
+                employee.Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+                employee.Id = i.ToString();
+                Resources.Add(employee);
+            }
+        }
+
+        private void GenerateRandomAppointments()
+        {
+            var subjectCollection = new List<string>();
+            subjectCollection.Add("General Meeting");
+            subjectCollection.Add("Plan Execution");
+            subjectCollection.Add("Project Plan");
+            subjectCollection.Add("Consulting");
+            subjectCollection.Add("Performance Check");
+            subjectCollection.Add("Yoga Therapy");
+            subjectCollection.Add("Plan Execution");
+            subjectCollection.Add("General Meeting");
+            subjectCollection.Add("Consulting");
+            subjectCollection.Add("Performance Check");
+
+            Random randomTime = new Random();
+            Events = new ScheduleAppointmentCollection();
+            DateTime date;
+            DateTime dateFrom = DateTime.Now.AddDays(-80);
+            DateTime dateTo = DateTime.Now.AddDays(80);
+            DateTime dateRangeStart = DateTime.Now.AddDays(-70);
+            DateTime dateRangeEnd = DateTime.Now.AddDays(70);
+
+			for (date = dateFrom; date < dateTo; date = date.AddDays(1))
+			{
+				if ((DateTime.Compare(date, dateRangeStart) > 0) && (DateTime.Compare(date, dateRangeEnd) < 0))
+				{
+					for (int additionalAppointmentIndex = 0; additionalAppointmentIndex < 8; additionalAppointmentIndex++)
+					{
+						ScheduleAppointment meeting = new ScheduleAppointment();
+						meeting.StartTime = new DateTime(date.Year, date.Month, date.Day, randomTime.Next(9, 15), 0, 0);
+						meeting.EndTime = meeting.StartTime.AddHours(randomTime.Next(1, 3));
+						meeting.Subject = subjectCollection[randomTime.Next(9)];
+						meeting.AppointmentBackground = this.brushes[randomTime.Next(9)];
+						meeting.IsAllDay = false;
+
+						var coll = new ObservableCollection<object>
+			{
+				(resources[randomTime.Next(Resources.Count)] as SchedulerResource).Id
+
+							};
+			meeting.ResourceIdCollection = coll;
+
+			this.Events.Add(meeting);
+		}
+
+                    for (int additionalAppointmentIndex = 0; additionalAppointmentIndex< 8; additionalAppointmentIndex++)
+                    {
+                        ScheduleAppointment meeting = new ScheduleAppointment();
+		meeting.StartTime = new DateTime(date.Year, date.Month, date.Day, randomTime.Next(10, 12), 0, 0);
+                        meeting.EndTime = meeting.StartTime.AddHours(randomTime.Next(1, 3));
+                        meeting.Subject = subjectCollection[randomTime.Next(9)];
+                        meeting.AppointmentBackground = this.brushes[randomTime.Next(9)];
+                        meeting.IsAllDay = false;
+
+                        var coll = new ObservableCollection<object>
+							{
+								(resources[randomTime.Next(Resources.Count)] as SchedulerResource).Id
+							};
+		meeting.ResourceIdCollection = coll;
+
+                        this.Events.Add(meeting);
+                    }
+}
+
+				else
+{
+	ScheduleAppointment meeting = new ScheduleAppointment();
+	meeting.StartTime = new DateTime(date.Year, date.Month, date.Day, randomTime.Next(0, 23), 0, 0);
+	meeting.EndTime = meeting.StartTime.AddDays(2).AddHours(1);
+	meeting.Subject = subjectCollection[randomTime.Next(9)];
+	meeting.AppointmentBackground = this.brushes[randomTime.Next(9)];
+	meeting.IsAllDay = true;
+	var coll = new ObservableCollection<object>
+							{
+								(resources[randomTime.Next(Resources.Count)] as SchedulerResource).Id
+							};
+	meeting.ResourceIdCollection = coll;
+	this.Events.Add(meeting);
+}
+            }
+
+            ScheduleAppointment meeting2 = new ScheduleAppointment();
+            meeting2.StartTime = DateTime.Now.Date.AddHours(10);
+            meeting2.EndTime = meeting2.StartTime.AddHours(2);
+            meeting2.Subject = subjectCollection[1];
+            meeting2.AppointmentBackground = this.brushes[1];
+            var col2 = new ObservableCollection<object>
+                            {
+                                "1"
+							};
+            meeting2.ResourceIdCollection = col2;
+            this.Events.Add(meeting2);
+
+            ScheduleAppointment meeting3 = new ScheduleAppointment();
+            meeting3.StartTime = DateTime.Now.Date.AddHours(11);
+            meeting3.EndTime = meeting3.StartTime.AddHours(2);
+            meeting3.Subject = subjectCollection[2];
+            meeting3.AppointmentBackground = this.brushes[1];
+            var col3 = new ObservableCollection<object>
+                            {
+                                "1"
+							};
+            meeting3.ResourceIdCollection = col3;
+            this.Events.Add(meeting3);
+        }
+
+        private void GetBrushCollection()
+		{
+            brushes = new ObservableCollection<SolidColorBrush>()
+            {
+            new SolidColorBrush(Color.FromArgb(255, 157, 101, 201)),
+            new SolidColorBrush(Color.FromArgb(255, 240, 138, 93)),
+            new SolidColorBrush(Color.FromArgb(255, 103, 155, 155)),
+            new SolidColorBrush(Color.FromArgb(255, 141,34,45)),
+            new SolidColorBrush(Color.FromArgb(255, 22,146,22)),
+            new SolidColorBrush(Color.FromArgb(255, 12,22,216)),
+            new SolidColorBrush(Color.FromArgb(255, 216,145,12)),
+            new SolidColorBrush(Color.FromArgb(255, 177,7,191)),
+            new SolidColorBrush(Color.FromArgb(255, 39,170,254)),
+            };
+        }
+    }
+}
