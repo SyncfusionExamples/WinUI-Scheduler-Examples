ec915d49afb9b1efcd9e559ae0b7513c6530dec9
Naveenkumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2022-04-01 16:40:32 +0530

    Added fare calendar sample demo

---
+++ FareCalendar/FareCalendar/Model/Airline.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.UI.Xaml.Media;
+using Syncfusion.UI.Xaml.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FareCalendar
+{
+    public class Airline : NotificationObject
+    {
+        private DateTime from;
+        private Brush color;
+        private string name;
+        private string fare;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return from; }
+            set
+            {
+                from = value;
+                RaisePropertyChanged("Date");
+            }
+        }
+
+        public string Fare
+        {
+            get { return fare; }
+            set
+            {
+                fare = value;
+                RaisePropertyChanged("Fare");
+            }
+        }
+
+        public Brush Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                RaisePropertyChanged("Color");
+            }
+        }
+    }
+}
---
+++ FareCalendar/FareCalendar/ViewModel/AirlinesViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FareCalendar
+{
+	public class AirlinesViewModel
+	{
+		/// <summary>
+		/// Initializes a new instance of the AirlinesViewModel class.
+		/// </summary>
+		public AirlinesViewModel()
+		{
+			GetAirlinesData();
+		}
+
+		/// <summary>
+		/// Gets or sets the airline collection.
+		/// </summary>
+		public ObservableCollection<Airline> AirlineCollection { get; set; }
+
+		/// <summary>
+		/// Methods to get the airlines data.
+		/// </summary>
+		private void GetAirlinesData()
+		{
+			List<string> Fares = new List<string>()
+			 {   "$134.50", "$305.00", "$152.66", "$267.09", "$189.20", "$212.10", "$238.83",
+				 "$100.17", "$101.72", "$332.48" ,"$100.17", "$449.68", "$378.44", "$273.45",
+				 "$134.50", "$305.00", "$152.66", "$267.09", "$189.20", "$212.10", "$238.83",
+			};
+
+			List<string> airlineNames = new List<string>() { "Airways 1", "Airways 2", "Airways 3" };
+			Random random = new Random();
+
+			var startDate = new DateTime(DateTime.Now.Date.Year, DateTime.Now.Date.Month, 1);
+			//// Creating an instance for airline collection.
+			this.AirlineCollection = new ObservableCollection<Airline>();
+			//// Adding random airline data to the airline collecion.
+			for (int i = 0; i < 300; i++)
+			{
+				var airline = new Airline();
+				airline.Name = airlineNames[random.Next(0, 3)];
+				airline.Fare = Fares[random.Next(0, 20)];
+				airline.Color = GetAirlineColor(airline.Name);
+				airline.Date = startDate.AddDays(i);
+				this.AirlineCollection.Add(airline);
+			}
+		}
+
+		/// <summary>
+		/// Methods to get the airline color.
+		/// </summary>
+		/// <param name="airlineName"></param>
+		/// <returns></returns>
+		private Brush GetAirlineColor(string airlineName)
+		{
+			if (airlineName == "Airways 1")
+				return new SolidColorBrush(Colors.Red);
+			else if (airlineName == "Airways 2")
+				return new SolidColorBrush(Colors.Green);
+			else
+				return new SolidColorBrush(Colors.Gray);
+		}
+	}
+}
