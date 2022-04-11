7a3009b88677efbb5569022d7dd75cb4eb253737
Naveenkumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2022-04-11 18:22:06 +0530

    Renamed FareCalendar into FareCalendar_DataTemplateSelector

--- FareCalendar/FareCalendar_DataTemplateSelector/Model/Airline.cs
+++ FareCalendar/FareCalendar_DataTemplateSelector/Model/Airline.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FareCalendar
+namespace FareCalendar_DataTemplateSelector
 {
     public class Airline : NotificationObject
     {
--- FareCalendar/FareCalendar_DataTemplateSelector/ViewModel/AirlinesViewModel.cs
+++ FareCalendar/FareCalendar_DataTemplateSelector/ViewModel/AirlinesViewModel.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FareCalendar
+namespace FareCalendar_DataTemplateSelector
 {
 	public class AirlinesViewModel
 	{
